538c0648d76b16deb17d107d9885f57354277ea8
IA1r <bakudo81.test@gmail.com>
2022-07-15 22:03:49 +0300

    Init

---
+++ HiraganaKatakana2/Data/ArrayExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace HiraganaKatakana2.Data
+{
+    public static class ArrayExtension
+    {
+        private static readonly Random rng = new();
+
+        public static void Shuffle<T>(this T[] array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = array[k];
+                array[k] = array[n];
+                array[n] = value;
+            }
+        }
+    }
+}
